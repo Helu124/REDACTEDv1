3c82da589ec1e49ba5ce7e3532db35787637d436
Helu124 <eliasrebeh@gmail.com>
2025-03-01 01:11:43 +0100

    vite changed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/REDACTEDv1/',  // Change this to match your repo name
+  base: '/',  // Change this to match your repo name
   plugins: [react()],
 });
 
